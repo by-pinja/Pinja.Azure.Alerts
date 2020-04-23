2e77a5ec2520fecf1c59003622d6063b9b644de5
Savpek <savolainen.pekka@gmail.com>
2020-04-23 14:42:19 +0300

    Added create parent dir for doc generator scipt

--- misc/GenerateDefaultAlertDocMd.ps1
+++ misc/GenerateDefaultAlertDocMd.ps1
@@ -3,6 +3,13 @@ Param([string]$OutPath)
 
 . $PSScriptRoot/../src/Get-DefaultAlertRules.ps1
 
+$parentFolder = (Split-Path -Path $OutPath -Parent)
+
+if(-not (Test-Path $parentFolder))
+{
+    New-Item -ItemType Directory $parentFolder -Force
+}
+
 $asText = Get-DefaultAlertRules | &"$PSScriptRoot/ConvertTo-Markdown.ps1"
 
 $asText | Set-Content -Path $OutPath
