737c8545e152a278fd7b9396df66e0a0e51d4446
Savpek <savolainen.pekka@gmail.com>
2020-04-24 16:39:04 +0300

    Generate documents from powershell module

--- misc/GenerateModuleDocumentation.ps1
+++ misc/GenerateModuleDocumentation.ps1
@@ -1,4 +1,15 @@
+[CmdLetBinding()]
+Param(
+    [string][Parameter()]$OutPath
+)
+
+if(-not $OutPath)
+{
+    $OutPath = Resolve-Path "$PSScriptRoot/../temp/"
+}
+
+
 Remove-Module Pinja.Azure.Alerts
 Import-Module $PSScriptRoot/../src/Pinja.Azure.Alerts.psd1
 
-New-MarkdownHelp -Module Pinja.Azure.Alerts -OutputFolder $PSScriptRoot/../temp/ -Force
+New-MarkdownHelp -Module Pinja.Azure.Alerts -OutputFolder $OutPath -Force
