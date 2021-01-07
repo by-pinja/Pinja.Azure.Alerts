950e0ed53a9e992fd4ed005e3ee61ae1ac4b7e3e
Savpek <savolainen.pekka@gmail.com>
2021-01-07 15:11:55 +0200

    Updates to support github action builds

--- Publish.ps1
+++ Publish.ps1
@@ -1,8 +1,12 @@
 [CmdletBinding()]
 Param(
-    [string][Parameter(Mandatory)]$Version,
     [string][Parameter(Mandatory)]$NuGetApiKey
 )
 
-Update-ModuleManifest -Path $PSScriptRoot/Pinja.Azure.Alerts/Pinja.Azure.Alerts.psd1 -ModuleVersion $Version
+if($env:GITHUB_REF) {
+    $version = $env:GITHUB_REF -split "/" | select-object -last 1
+    Write-Host "Found github ref, parsed version $version and updating it to module"
+    Update-ModuleManifest -Path $PSScriptRoot/Pinja.Azure.Alerts/Pinja.Azure.Alerts.psd1 -ModuleVersion $Version
+}
+
 Publish-Module -Path $PSScriptRoot/Pinja.Azure.Alerts -NuGetApiKey $NuGetApiKey -WhatIf
